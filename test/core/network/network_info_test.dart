a786bb1691b1b9c95840c6b57a55f9ff9fb6c6af
prince-438 <mnisiddique30@gmail.com>
2021-09-28 16:35:35 +0600

    Completes tutorial part-07  by implementing internet availability checking.

---
+++ test/core/network/network_info_test.dart
@@ -0,0 +1,34 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:number_trivia/core/network/network_info.dart';
+
+class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
+
+void main() {
+  NetworkInfoImpl networkInfoImpl;
+  MockDataConnectionChecker mockDataConnectionChecker;
+
+  setUp(() {
+    mockDataConnectionChecker = MockDataConnectionChecker();
+    networkInfoImpl = NetworkInfoImpl(mockDataConnectionChecker);
+  });
+
+  group("Network Info Test: ", () {
+    test(
+        "Network info should forward the call to DataConnectionChecker.hasConnection",
+        () async {
+      //arrange
+      final tHasConnectionFuture = Future.value(true);
+      when(mockDataConnectionChecker.hasConnection)
+          .thenAnswer((realInvocation) => tHasConnectionFuture);
+
+      //act
+      final result = networkInfoImpl.isConnected;
+
+      //assert
+      verify(mockDataConnectionChecker.hasConnection);
+      expect(result, tHasConnectionFuture);
+    });
+  });
+}
